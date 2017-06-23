e7bcab2178c9363f3c8aa17bff3f4caf6ef0d94c
BigBeardy <westderick70@gmail.com>
2017-06-23 16:19:24 -0400

    names thing

---
+++ names.rb
@@ -0,0 +1,43 @@
+# 1.times do
+# name = 1
+# 	name is my variable 
+# 	if name == 1 
+# 	p "my first name is derick"
+# 	elsif name ==2
+# 		p "my first name is Tori"
+# 	else name == 3
+# 		p "my first name is chris"
+# 	end
+# 		if name ==1
+# 			p "my last name is west"
+# 		elsif name ==2
+# 			p "my last name is frame"
+# 		else name == 3
+# 			p "my last name is phelps"
+# 		end
+
+# name =+ 1
+# end
+
+def firstname(name)
+	print "My first name is " + name
+end
+def lastname(name) 
+	print " and my last name is #{name}"
+end
+ firstname("derick")
+ lastname("west")
+
+# def firstname
+# 	 "My first name is derick"
+# 	end
+# def lastname 
+# 	 "my last name is west"
+# 	end
+# p firstname
+# p lastname
+	
+
+
+
+
